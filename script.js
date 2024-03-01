45b3d890edcddbb9d7560bb4e304cfb2720f6d61
marcelofq-dev <chinesio84@gmail.com.br>
2024-03-01 15:39:54 -0300

    adding touch function

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function toggleMode() {
-	const html = document.documentElement
-	html.classList.toggle("light")
+const themeSwitchBtn = document.querySelector("#switch button")
+const themeSwitch = document.querySelector("#switch")
+let trackIsMoving = false
 
+themeSwitchBtn.addEventListener("touchmove", moveTrack)
+themeSwitchBtn.addEventListener("touchend", trackTouchEnd)
+themeSwitch.addEventListener("click", toggleTheme)
+
+// Theme toggle function
+function toggleTheme() {
+	const html = document.documentElement
 	const img = document.querySelector("#profile img")
+	html.classList.toggle("light")
 
 	if (html.classList.contains("light")) {
 		img.setAttribute("src", "./assets/avatar-light.png")
@@ -10,5 +18,17 @@ function toggleMode() {
 	} else {
 		img.setAttribute("src", "./assets/avatar.png")
 		img.setAttribute("alt", "Marcelo de óculos sorrindo com um fundo laranja")
+	}
+}
+
+function moveTrack(event) {
+	trackIsMoving = true
+}
+
+// Change theme color when touch ends
+function trackTouchEnd() {
+	if (trackIsMoving) {
+		trackIsMoving = false
+		toggleTheme()
 	}
 }
